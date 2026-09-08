72901ccc0439c4d84cd1e60e3303dd5ff2c3ec26
HardikMarlapudi <hardik.a.marlapudi@gmail.com>
2026-09-08 03:11:43 -0400

    added a border-radius feature

--- digitalClock/src/components/DigitalClock.jsx
+++ digitalClock/src/components/DigitalClock.jsx
@@ -28,9 +28,9 @@ const DigitalClock = () => {
 }, []);
 
     return (
-        <div className="bg-black px-20 py-60">
-            <div className="flex justify-center align-center text-center bg-gray-900 relative border-4">
-                <div className="flex justify-center align-center w-auto text-7xl font-sans font-bold px-10 py-35 text-green-500">{formattedTime}</div>
+        <div className="bg-black rounded-4xl px-20 py-60">
+            <div className="flex justify-center align-center text-center bg-gray-900 rounded-4xl relative border-4">
+                <div className="flex justify-center align-center w-auto text-7xl font-sans rounded-4xl font-bold px-10 py-35 text-green-500">{formattedTime}</div>
             </div>
         </div>
     )
